b983023a8e9a2795d18f774b5533a1f5c7039243
Gatanot <baoyuedaocun@gmail.com>
2026-05-30 15:21:07 +0800

    fix: 主页不再自动跳转到首个会话，Qualia品牌点击回到主页

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	import { sessions, loadSessions } from '$lib/session-store';
+	import { loadSessions } from '$lib/session-store';
 	import EmptyState from '$lib/components/EmptyState.svelte';
 
 	$effect(() => {
-		loadSessions().then(() => {
-			if ($sessions.length > 0) {
-				goto('/chat/' + $sessions[0].id, { replaceState: true });
-			}
-		});
+		loadSessions();
 	});
 </script>
 
